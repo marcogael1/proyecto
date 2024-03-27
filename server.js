bdb7840a29609c2bb176c468c8886bf5fe254d51
marcogael1 <132959641+marcogael1@users.noreply.github.com>
2024-03-26 20:29:35 -0600

    marco

--- server.js
+++ server.js
@@ -680,5 +680,5 @@ app.post('/cajas-fuertes', async (req, res) => {
 
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  //console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
